3c028d010666d75bbd5978c39f801532ed881afc
banana0816 <Zzhu@putnamscience.org>
2025-04-08 14:17:42 -0400

    1

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,19 +2,17 @@ import random
 
 def escape_game():
     position = 0
-    goal = 100
+    goal = 30
     supplies = 200
 
-    Print ("Wellcome to the escape game, you need to get from position 0 to position 100. You may encouter danger along the way!")
+    Print ("Wellcome to the escape game, you need to get from position 0 to position 30. You may encouter danger along the way!")
 
     while position < goal:
         try:
             step = int(input("please enter the namber of steps forward(1-3): "))
             if step not in [1, 2, 3]:
                 print("please enter number between 1-3! ")
-                continue
-
-            
+                continue 
             
             position += step
             print ("You have moved {step} steps forward, current position:{position}")
@@ -32,15 +30,250 @@ def fire_challenge():
     for idx, opt in enumerate(options):
         print(f"{idx+1}. {opt}")
 
-
-    signal.signal(signal.SIGNAL, timeout_handler) 
-    signal.araim(3)
     try:
         answer = input("please enter the option number (1-3):")
+        signal.alarm()
+        if answer.isdigit() and int(answer) in [1, 2, 3]:
+            if options[int(answer)-1] == '水'
+               print("choose the right one and you move on.")
+               return True
+            else:
+                print("wrong choice.")
+                return False
+
+        else:
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
         
-
-
-
         
 
 
